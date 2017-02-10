e41885ea32ab0d7e9476fe5019864de347cd8b12
Joe Ledger <joewledger@gmail.com>
2017-02-09 21:40:01 -0500

    Finalized code for building a user-item matrix from the movie-lens dataset
    Signed-off-by: Joe Ledger <joewledger@gmail.com>

--- algorithm_server/io_utils.py
+++ algorithm_server/io_utils.py
@@ -1,4 +1,30 @@
-from bidict import bidict
+from scipy.sparse import *
+
+
+class User_Movie_Matrix:
+
+    def __init__(self):
+        #Map the user and movie ids from the MovieLens dataset to indices in the User-Movie matrix
+        self.user_id_index = {}
+        self.movie_id_index = {}
+
+        self.temp_storage_dict = {}
+        self.matrix = None
+
+    def add_rating(self, user, movie, rating):
+        self.update_index(self.user_id_index, user)
+        self.update_index(self.movie_id_index, movie)
+        self.temp_storage_dict[(user, movie)] = rating
+
+    def update_index(self, index, identifier):
+        if(identifier not in index):
+            index[identifier] = len(index)
+
+    def build_matrix(self):
+        self.matrix = dok_matrix((len(self.user_id_index), len(self.movie_id_index)))
+        for user, movie in self.temp_storage_dict:
+            self.matrix[self.user_id_index[user], self.movie_id_index[movie]] = self.temp_storage_dict[(user, movie)]
+        self.matrix = self.matrix.tocsr()
 
 
 def ratings_file(datadir):
@@ -8,7 +34,9 @@ def ratings_file(datadir):
 def movie_description_file(datadir):
     return "%s/movies.csv" % datadir
 
-
+"""
+Returns a generator in which each item is a tuple with the following fields ("userId", "movieId", "rating")
+"""
 def get_ratings_stream(datadir):
     reader = open(ratings_file(datadir), "r")
     for i, line in enumerate(reader.readlines()):
@@ -17,28 +45,14 @@ def get_ratings_stream(datadir):
     reader.close()
 
 
-def map_movie_id_to_matrix_location(datadir):
-    matrix_loc_map = bidict()
+def build_user_item_matrix(datadir):
+    matrix = User_Movie_Matrix()
 
-    reader = open(movie_description_file(datadir), "r")
-    for i, line in enumerate(reader.readlines()):
-        if(i >= 1):
-            comma_pos = line.find(",")
-            if(comma_pos > 0):
-                matrix_loc_map[line[:comma_pos]] = i
+    for userId, movieId, rating in get_ratings_stream(datadir):
+        matrix.add_rating(int(userId), int(movieId), float(rating))
 
-    return matrix_loc_map
+    matrix.build_matrix()
+    return matrix
 
-
-def build_user_movie_matrix_as_dict(datadir):
-    user_movie_dict = dict()
-    unique_users = set()
-
-    movie_id_locs = map_movie_id_to_matrix_location(datadir)
-    ratings_stream = get_ratings_stream(datadir)
-
-    for user_id, movie_id, rating in ratings_stream:
-        user_movie_dict[(int(user_id) + len(movie_id_locs), movie_id_locs[movie_id])] = rating
-        unique_users.add(user_id)
-
-    return len(unique_users) + len(movie_id_locs), user_movie_dict
+m = build_user_item_matrix("../data-small")
+print(m.matrix)
--- algorithm_server/recommendations.py
+++ algorithm_server/recommendations.py
@@ -1,36 +1,16 @@
 import io_utils
 import scipy.sparse as sp
 import sklearn.preprocessing as sk
+import numpy as np
 
 
-class Ratings_Matrix:
+def calculate_user_similarities(ratings_matrix, new_user_reviews):
+    num_users = ratings_matrix.shape[0]
 
-    def __init__(self):
-        self.initialized = False
-        self.positive_rating_matrix = None
-        self.negative_rating_matrix = None
+    user_similarities = np.array()
+    for i in range(num_users):
 
-    def load_from_file(self, datadir):
-        dimension, user_movies_ratings_dict = io_utils.build_user_movie_matrix_as_dict(datadir)
+        row = ratings_matrix.getrow(i)
+        print(type(row))
 
-        self.positive_rating_matrix = sp.dok_matrix((dimension, dimension))
-        self.negative_rating_matrix = sp.dok_matrix((dimension, dimension))
-        for key in user_movies_ratings_dict.keys():
-            user, movie = key
-            rating = float(user_movies_ratings_dict[key])
-
-            if(rating < 3.0):
-                self.negative_rating_matrix[user, movie] = 1.0
-                self.negative_rating_matrix[movie, user] = 1.0
-            else:
-                self.positive_rating_matrix[user, movie] = 1.0
-                self.positive_rating_matrix[movie, user] = 1.0
-
-        self.positive_rating_matrix = sk.normalize(self.positive_rating_matrix.tocsr(), norm="l1")
-        self.negative_rating_matrix = sk.normalize(self.negative_rating_matrix.tocsr, norm="l1")
-
-
-
-    #user_preferences should be a dictionary mapping {movie_id -> rating}
-    def make_recommendations(self, user_preferences):
-        return None
+    return user_similarities
