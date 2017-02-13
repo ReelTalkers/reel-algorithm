87b27d163b05f2196168352033f754c9813cd093
Joe Ledger <joewledger@gmail.com>
2017-02-13 13:47:05 -0500

    Added method to convert user preferences with movielens id indices to user preferences with matrix indices
    Signed-off-by: Joe Ledger <joewledger@gmail.com>

--- algorithm_server/io_utils.py
+++ algorithm_server/io_utils.py
@@ -9,7 +9,22 @@ class User_Movie_Matrix:
         self.movie_id_index = {}
 
         self.temp_storage_dict = {}
-        self.matrix = None
+        self.matrix = dok_matrix((1, 1))
+
+    def get_shape(self):
+        return self.matrix.shape
+
+    def get_sparse_ratings_vector(self, preferences):
+        """
+        Converts a user's movie ratings using movie lens identifiers to an index
+        based on the locations of movies in the User_Movie_Matrx
+
+        @Param preferences: A mapping of (movieLens movie id) -> (user rating)
+
+        Returns a dictionary with the same user ratings but instead mapping (matrix location) -> (user rating)
+        Useful when we need to get user similarity profiles.
+        """
+        return {self.movie_id_index[x]: preferences[x] for x in preferences}
 
     def add_rating(self, user, movie, rating):
         self.update_index(self.user_id_index, user)
@@ -22,8 +37,11 @@ class User_Movie_Matrix:
 
     def build_matrix(self):
         self.matrix = dok_matrix((len(self.user_id_index), len(self.movie_id_index)))
-        for user, movie in self.temp_storage_dict:
-            self.matrix[self.user_id_index[user], self.movie_id_index[movie]] = self.temp_storage_dict[(user, movie)]
+        for key, value in list(self.temp_storage_dict.items()):
+            user, movie = key
+            self.matrix[self.user_id_index[user], self.movie_id_index[movie]] = value
+            del(self.temp_storage_dict[key])
+        del(self.temp_storage_dict)
         self.matrix = self.matrix.tocsr()
 
 
@@ -34,15 +52,14 @@ def ratings_file(datadir):
 def movie_description_file(datadir):
     return "%s/movies.csv" % datadir
 
+
 """
 Returns a generator in which each item is a tuple with the following fields ("userId", "movieId", "rating")
 """
 def get_ratings_stream(datadir):
-    reader = open(ratings_file(datadir), "r")
-    for i, line in enumerate(reader.readlines()):
+    for i, line in enumerate(open(ratings_file(datadir), "r")):
         if(i >= 1):
             yield tuple(str(line).strip().split(",")[:3])
-    reader.close()
 
 
 def build_user_item_matrix(datadir):
@@ -51,8 +68,13 @@ def build_user_item_matrix(datadir):
     for userId, movieId, rating in get_ratings_stream(datadir):
         matrix.add_rating(int(userId), int(movieId), float(rating))
 
+    print("Done adding ratings")
+
     matrix.build_matrix()
     return matrix
 
-m = build_user_item_matrix("../data-small")
-print(m.matrix)
+
+if __name__ == "__main__":
+
+    m = build_user_item_matrix("../data-small")
+    print(m.matrix)
